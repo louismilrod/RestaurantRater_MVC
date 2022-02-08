0712c8106c55c260b76f73d4715b42be2e9c3802
Louis Milrod <int4rweb@gmail.com>
2022-02-08 09:57:18 -0500

    added a delete function

--- RestaurantRater_MVC/Controllers/RestaurantController.cs
+++ RestaurantRater_MVC/Controllers/RestaurantController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -36,5 +37,34 @@ namespace RestaurantRater_MVC.Controllers
             }
             return View();
         }
+
+        //GET: Restaurant/Delete/{id}
+        public ActionResult Delete (int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(restaurant);
+        }
+
+        //POST: Restaurant/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
